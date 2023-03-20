666eb1556c03172dab4e72076b19301293e9a4b2
Alexander <32470113+dojekon@users.noreply.github.com>
2023-03-20 22:59:07 +0300

    Made method to get elements by tab

--- src/main/java/com/imgdownloader/App.java
+++ src/main/java/com/imgdownloader/App.java
@@ -1,8 +1,17 @@
 package com.imgdownloader;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.jsoup.nodes.Element;
+
 public class App {
 
-  public static void main(String[] args) {
-    System.out.println("Hello from the main method");
+  public static void main(String[] args) throws IOException {
+    ImgDownloader downloader = new ImgDownloader("https://nextcloud.dojekon.ru/", null, null);
+    List<Element> URLs = downloader.getPageElementsByTag(downloader.getUrl(), "a");
+    for (Element element : URLs) {
+      System.out.println(element.attr("href"));
+    }
   }
 }
--- src/main/java/com/imgdownloader/ImgDownloader.java
+++ src/main/java/com/imgdownloader/ImgDownloader.java
@@ -1,13 +1,32 @@
 package com.imgdownloader;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.*;
+
+
 public class ImgDownloader {
-    String url;
-    String savePath;
-    String imgMinSize;
+    private String url;
+    private String savePath;
+    private String imgMinSize;
 
     public ImgDownloader(String url, String savePath, String imgMinSize) {
         this.url = url;
         this.savePath = savePath;
         this.imgMinSize = imgMinSize;
+    }
+
+    String getUrl() { return this.url; };
+    String getSavePath() { return this.savePath; }
+    String getImgMinSizeS() { return this.imgMinSize; }
+
+    public List<Element> getPageElementsByTag(String url, String tag) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        return doc.select(tag);
     }
 }
