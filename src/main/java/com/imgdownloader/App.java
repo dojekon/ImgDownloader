48eb361c8f17dd947fb0bb2cedf9eb13757ff03a
Alexander <32470113+dojekon@users.noreply.github.com>
2023-04-07 00:14:40 +0300

    Made multithreading

--- src/main/java/com/imgdownloader/App.java
+++ src/main/java/com/imgdownloader/App.java
@@ -1,14 +1,13 @@
 package com.imgdownloader;
 
-import java.util.Set;
 
 public class App {
 
   public static void main(String[] args) {
-    ImgDownloader downloader = new ImgDownloader("https://vk.com/", null, null);
-    Set<String> links = downloader.getImgLinks("https://browsershots.org/");
-    for (String string : links) {
-      System.out.println(string);
-    }
+    ImgDownloader downloader = new ImgDownloader("https://mail.ru/", "C:/Users/dojekon/Documents/GitHub/img", 0);
+     // Вывод статистики каждую секунду
+    downloader.getAndDownloadImagesMultithread();
+
+
   }
 }
--- src/main/java/com/imgdownloader/ImgDownloader.java
+++ src/main/java/com/imgdownloader/ImgDownloader.java
@@ -1,27 +1,49 @@
 package com.imgdownloader;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.InputStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+
 
 
 public class ImgDownloader {
     private String url;
     private String savePath;
-    private String imgMinSize;
+    private int imgMinSize;
+    private final AtomicInteger downloadedImages = new AtomicInteger(0);
+    private final AtomicReference<String> currentImage = new AtomicReference<>("");
+
 /** */
-    public ImgDownloader(String url, String savePath, String imgMinSize) {
-        this.url = url;
+    public ImgDownloader(String url, String savePath, int imgMinSizeKB) {
+      /* if (url == null || url.isEmpty()) {
+        throw new IllegalArgumentException("URL cannot be empty");
+       } */
+       /*if (savePath == null || savePath.isEmpty()) {
+        throw new IllegalArgumentException("savePath cannot be empty"); 
+       }*/
+       this.url = url;
         this.savePath = savePath;
-        this.imgMinSize = imgMinSize;
+        this.imgMinSize = imgMinSizeKB * 1024;
     }
 
     String getUrl() { return this.url; };
     String getSavePath() { return this.savePath; }
-    String getImgMinSizeS() { return this.imgMinSize; }
+    int getImgMinSizeS() { return this.imgMinSize; }
 
     /**
      * Получает список элементов на странице по заданному тегу элемента
@@ -48,6 +70,7 @@ public class ImgDownloader {
         List<Element> elements = getPageElementsByTag(url, "a");
 
         for (Element element : elements) {
+            // TODO Make abs:href obj
             if (!element.attr("abs:href").toString().equals(url) && !element.attr("abs:href").toString().equals("")) {
             links.add(element.attr("abs:href").toString());
             }
@@ -85,4 +108,116 @@ public class ImgDownloader {
 
         return imgLinks;
     }
+
+    public void downloadImg(String imageUrl) { 
+        if (!isImageSizeValid(imageUrl)) {
+            return;
+        }     
+
+        // Обновляем информацию о текущей картинке
+        currentImage.set(imageUrl);
+        try {
+            URL url = new URL(imageUrl);
+            InputStream is = url.openStream();
+            byte[] buffer = new byte[4096];
+            int length;
+            // Создаем файл на диске и записываем в него изображение
+            String filename = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+            // Обрезаем аргументы
+            if (filename.contains("?")) {
+                filename = filename.substring(0, filename.indexOf(("?")));
+            }
+            FileOutputStream fos = new FileOutputStream(this.getSavePath() + "/" + filename
+            );
+            while ((length = is.read(buffer)) > 0) {
+                fos.write(buffer, 0, length);
+            }
+
+            is.close();
+            fos.close();
+
+            // Увеличиваем количество скачанных изображений
+             downloadedImages.incrementAndGet();
+            
+        } catch (IOException e) {
+          //  e.printStackTrace();
+        }
+    }
+
+    public void downloadImg(Set<String> imageUrls) {
+        for (String imageUrl : imageUrls) {
+            downloadImg(imageUrl);
+        }
+    }
+
+    public void downloadImgMultithread(Set<String> imageUrls) {
+        // Создайте пул потоков с фиксированным количеством потоков, например, 10
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+    
+        // Отправьте задачи на скачивание изображений в пул потоков
+        for (String imageUrl : imageUrls) {
+            executorService.submit(() -> downloadImg(imageUrl));
+        }
+    
+        // Завершите работу пула потоков после отправки всех задач на скачивание
+        executorService.shutdown();
+    
+        // Ожидайте завершения всех задач на скачивание или прерывания по таймауту, например, 10 минут
+        try {
+            if (!executorService.awaitTermination(10, TimeUnit.MINUTES)) {
+                // Если не все задачи успели завершиться, прерываем оставшиеся задачи
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            // Если ожидание прервано, прерываем оставшиеся задачи
+            executorService.shutdownNow();
+        }
+    }
+    
+
+    public void getAndDownloadImages() {
+        Set<String> siteLinks = getChildLinksFPage(this.url);
+        siteLinks.add(this.getUrl());
+        downloadImg(getImgLinks(siteLinks));
+    }
+
+    public void getAndDownloadImagesMultithread() {
+        Set<String> siteLinks = getChildLinksFPage(this.url);
+        siteLinks.add(this.getUrl());
+        this.startStatisticsThread(1000);
+        downloadImgMultithread(getImgLinks(siteLinks));
+    }
+    /***
+     * Метод проверки размера скачиваемого изображения
+     * @param imageUrl
+     * @return
+     */
+    private boolean isImageSizeValid(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            URLConnection connection = url.openConnection();
+            int imageSize = connection.getContentLength(); // Размер изображения в байтах
+            return imageSize >= imgMinSize; // Возвращаем true, если размер изображения больше или равен минимальному размеру
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void startStatisticsThread(int intervalMs) {
+        // Создайте ScheduledExecutorService с одним потоком
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    
+        // Запустите задачу с заданным интервалом (intervalMs)
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            System.out.println("Текущее изображение: " + currentImage.get());
+            System.out.println("Всего скачано изображений: " + downloadedImages.get());
+        }, 0, intervalMs, TimeUnit.MILLISECONDS);
+    
+        // Остановите ScheduledExecutorService после завершения работы
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            scheduledExecutorService.shutdown();
+        }));
+    }
+    
 }
